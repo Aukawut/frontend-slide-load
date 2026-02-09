750f78dc9b1e0a296f3df6e504f52fead0b59b1a
Akawut <aukawut2542@gmail.com>
2026-02-09 07:55:21 +0700

    update

--- src/constants/main-data.ts
+++ src/constants/main-data.ts
@@ -1,6 +1,11 @@
 import type { PageConfig } from "./main-types";
 
 export const initialPages: PageConfig[] = [
+    {
+    id: "1",
+    name: "Digital Presence",
+    url: "http://digital-presence.psth.com/",
+  },
  
   {
     id: "2",
@@ -18,14 +23,15 @@ export const initialPages: PageConfig[] = [
     url: "http://10.144.1.100:9000/#!/5/docker/containers/4e5097688f095c1645bb5404f41a5ef7ce8b1acd317998f7309641d2f6ffbad4/stats",
   },
    {
-    id: "6",
+    id: "5",
     name: "Grafana",
     url: "http://10.144.1.90:3000/d/41URQF7mz/zabbix-full-server-status?orgId=1&from=now-6h&to=now&timezone=browser&var-Group=Zabbix%20servers&var-Host=Zabbix%20server&var-Disk=$__all&var-Filesystem=$__all&var-Network=$__all&refresh=auto",
   },
-  {
-    id: "7",
-    name: "Digital Presence",
-    url: "http://digital-presence.psth.com/",
-  }
+   {
+    id: "6",
+    name: "WMS",
+    url: "http://10.144.1.91/psth/pegasus/dashboard.php",
+  },
+
    
 ];
